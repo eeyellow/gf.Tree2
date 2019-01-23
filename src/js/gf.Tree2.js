9444adb6079912e0350309c8af2f88c069f4a8b2
Ray <rayhuang@geoforce.com.tw>
2019-01-23 12:28:23 +0800

    ✨ Feat :    新增歷史圖層按鈕與容器

--- src/js/gf.Tree2.js
+++ src/js/gf.Tree2.js
@@ -123,6 +123,10 @@
                 clear: {
                     src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAABqUlEQVQ4T53Uu+vPURzH8cfPfRMpE2WwMEgui5ESYmFxmaRcMlhsklgsNnL5ZXHJwsyAMsklyh+AkoWiTOTaS+fw+R2f7yWnPvX5nM95P9/v83pfJvxds/Gl8/1frxPFaiEe4Awme0gL8AMfRnmpwER3C5uxv4GuxU18x3Y8GwatwJzpgy4vgFkF8hmHcXkQtAtsoQdwCcdwqgEEGHAcTFktMD+X4TlmYhg0V48Er7vEFrgNVzC3HPo5Apok7cHtCu0C1+Ahpje3GAX9itV4Ebs2wk24jnk90IO42Gj6Hjtxry/CureklNDKEdCt2IE3wzRcjHflwDnsHQKNNEncCjzqi3AV7uJlJ3v7cLbUaLWJpvX6cXge6/C01XAaThSNPmI37iCO0kXfsAvHS0cdwgWsx33E0T9JyV7a71opnZPIM7+03iecxtECqNA/yvQVdn52E5MaS7TR7AY24HEZFBsxBToIGOgc1MS8wgwsKpodKSFFii1d6DBgvUZNTMZXJtHVTuYzNDKJEvVSvB0HGPskJh3xuxuaFWhK58mgpPTYjL81boRjE38B5LBgFUVPPZYAAAAASUVORK5CYII=',
                     desc: '清除圖層'
+                },
+                history: {
+                    src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAABqUlEQVQ4T53Uu+vPURzH8cfPfRMpE2WwMEgui5ESYmFxmaRcMlhsklgsNnL5ZXHJwsyAMsklyh+AkoWiTOTaS+fw+R2f7yWnPvX5nM95P9/v83pfJvxds/Gl8/1frxPFaiEe4Awme0gL8AMfRnmpwER3C5uxv4GuxU18x3Y8GwatwJzpgy4vgFkF8hmHcXkQtAtsoQdwCcdwqgEEGHAcTFktMD+X4TlmYhg0V48Er7vEFrgNVzC3HPo5Apok7cHtCu0C1+Ahpje3GAX9itV4Ebs2wk24jnk90IO42Gj6Hjtxry/CureklNDKEdCt2IE3wzRcjHflwDnsHQKNNEncCjzqi3AV7uJlJ3v7cLbUaLWJpvX6cXge6/C01XAaThSNPmI37iCO0kXfsAvHS0cdwgWsx33E0T9JyV7a71opnZPIM7+03iecxtECqNA/yvQVdn52E5MaS7TR7AY24HEZFBsxBToIGOgc1MS8wgwsKpodKSFFii1d6DBgvUZNTMZXJtHVTuYzNDKJEvVSvB0HGPskJh3xuxuaFWhK58mgpPTYjL81boRjE38B5LBgFUVPPZYAAAAASUVORK5CYII=',
+                    desc: '清除圖層'
                 }
 
             },
@@ -166,6 +170,14 @@
                 //子容器
                 var subContainer = $('<div/>').appendTo(o.target);
                 subContainer.height(o.target.height() - toolbar.outerHeight());
+
+
+                //歷史圖層
+                var historyContainer = $('<div/>', {
+                    'class': 'gfTreeHistoryContainer',
+                    'text': '1234'
+                }).appendTo(subContainer);
+
 
                 //搜尋工具容器
                 var searchContainer = $('<div/>', {
@@ -399,6 +411,16 @@
                     .on('click', '.gfTreeToolbar-Icon[data-type="list"]', function () {
                         o.target
                             .find('.gfTreeItemList')
+                            .show()
+                            .siblings()
+                            .hide()
+                    });
+
+                //工具 - 歷史圖層
+                o.target
+                    .on('click', '.gfTreeToolbar-Icon[data-type="history"]', function () {
+                        o.target
+                            .find('.gfTreeHistoryContainer')
                             .show()
                             .siblings()
                             .hide()
